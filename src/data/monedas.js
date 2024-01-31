9923ea7fc6c2c0a847a71b6d67bf9505df741da7
Jesica <jesicamachetti@Air-de-Jesica.fibertel.com.ar>
2024-01-31 16:25:05 -0300

    proyecto de cripto creado con react

---
+++ src/data/monedas.js
@@ -0,0 +1,13 @@
+const monedas = [
+
+    { id: 'USD', nombre: 'Dolar de Estados Unidos'},
+    { id: 'MXN', nombre: 'Peso Mexicano'},
+    { id: 'EUR', nombre: 'Euro'},
+    { id: 'ARG', nombre: 'Peso Argentino'},
+
+
+]
+
+export {
+    monedas
+}
